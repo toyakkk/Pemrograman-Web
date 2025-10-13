3212e5105af35ca60ea03ec692a692e89d055c84
@toya <athayaf47@gmail.com>
2025-10-13 22:50:03 +0800

    Add files via upload

---
+++ P6/01_struktur_dasar/komentar.php
@@ -0,0 +1,19 @@
+<?php
+echo "Hello World 1! </br>";
+?>
+
+<?php
+/*
+Berikut ini adalah perintah untuk menampilkan teks Hello World pada
+browser
+*/
+echo "Hello World 2! </br>";
+?>
+
+
+<?php
+// Berikut ini adalah perintah
+// untuk menampilkan teks Hello World
+// pada browser
+echo "Hello World 3!";
+?>
